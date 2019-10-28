e79a6aacb87cca6d88702e440fed6a2c6945e38c
roshad <roshad87a9@gmail.com>
2019-10-28 08:25:50 +0800

    removing moment

--- src/Card.js
+++ src/Card.js
@@ -1,50 +1,64 @@
 import React, { useState } from "react";
 import "./Card.css";
-import moment from "moment";
 import sound from "./alarm.mp3";
+
 function Card() {
   const [state, setState] = useState({
-    duration: moment.duration(),
-    showTime: 0
+    duration: 300000,
+    textBox: "5:0"
   });
-  
+  function durToShow(dur) {
+    return Math.floor(dur / 60000) + ":" + Math.floor((dur % 60000) / 1000);
+  }
+  function changeHandler(e) {
+    setState({ ...state, textBox: e.target.value });
+  }
   function submitHandler(e) {
     e.preventDefault();
     const matched = e.target
       .getElementsByTagName("input")[0]
       .value.match(/^(\d{1,2})[:.](\d{1,2})$/);
-    const pri_dur = moment.duration();
-    pri_dur.add(matched[1] * 1, "m");
-    pri_dur.add(matched[2] * 1, "s");
-    setState({...state, duration: pri_dur });
+    let pri_dur = matched[1] * 60000 + matched[2] * 1000;
+    setState({
+      ...state,
+      duration: pri_dur,
+      textBox: durToShow(pri_dur)
+    });
   }
   function ssHandler(e) {
-    const endTime = Date.now() + state.duration.asMilliseconds(),
-      audio = new Audio(sound),
+    let endTime = Date.now() + state.duration,
+      running = false;
+    const audio = new Audio(sound),
       countDown = setInterval(() => {
-        const remaining = endTime - Date.now();
-        let showTime = "";
-        showTime +=
-          Math.floor(remaining / 60000) +
-          ":" +
-          Math.floor((remaining % 60000) / 1000);
-        if (remaining <= 0) {
-          audio.play();
-          clearInterval(countDown);
+        if (state.running === true) {
+          let remaining = endTime - Date.now();
+          remaining = remaining < 0 ? 0 : remaining;
+          setState({
+            ...state,
+            textBox: durToShow(remaining)
+          });
+          if (remaining === 0) {
+            audio.play();
+            clearInterval(countDown);
+          }
         }
       }, 100);
+    if (running === false) {
+        endTime = Date.now() + state.duration
+
+    } else {
+      duration-=endTime-Date.now()
+    }
+    running =!running
   }
-  function changeHandler(e) {
-    setState({ showTime: e.target.value });
-    console.log(state.duration)
-  }
+
   return (
     <div class="timer_card">
       <form onSubmit={e => submitHandler(e)}>
         <input
           pattern="^\d{1,2}[:.]\d{1,2}$"
           onChange={e => changeHandler(e)}
-          value={state.showTime}
+          value={state.textBox}
         />
       </form>
       <div onClick={e => ssHandler(e)}>start</div>
