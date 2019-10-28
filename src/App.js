0788bbfe5e292ba57532ef2fed895cbe5d79142f
roshad <roshad87a9@gmail.com>
2019-10-28 21:39:53 +0800

    reset不中止倒计时？

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import './App.css';
-import Card from './Card1'
+import Card from './Card'
 function App() {
   return (
     <div>
--- src/Card.js
+++ src/Card.js
@@ -1,58 +1,64 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Card.css";
 import sound from "./alarm.mp3";
 
-function Card() {
-  const [state, setState] = useState({
+class Card extends React.Component {
+  state = {
+    duration:300000,
     textBox: "5:0",
-    running:false,
-    countDown:setInterval(() => {
-        if (state.running === true) {
-          let remaining = endTime - Date.now();
-          remaining = remaining < 0 ? 0 : remaining;
-          
-          setState({
-            ...state,
-            textBox: durToShow(remaining)
-          });
-          if (remaining === 0) {
-            audio.play();
-            clearInterval(state.countDown);
-          }
+    running: false,
+    endTime: null,
+    audio: new Audio(sound),
+    countDown: setInterval(() => {
+      if (this.state.running === true) {
+        let remaining = this.endTime - Date.now();
+        remaining = remaining < 0 ? 0 : remaining;
+
+        this.setState({
+          ...this.state,
+          textBox: this.durToShow(remaining)
+        });
+        if (remaining === 0) {
+          this.state.audio.play();
+          clearInterval(this.state.countDown);
         }
-      }, 100)
-  });
-  function durToShow(dur) {
+      }
+    }, 100)
+  };
+  durToShow(dur) {
     return Math.floor(dur / 60000) + ":" + Math.floor((dur % 60000) / 1000);
   }
-  function ShowToDur(text) {
+  ShowToDur(text) {
     const matched = text.match(/^(\d{1,2}):(\d{1,2})$/);
     return matched[1] * 60000 + matched[2] * 1000;
   }
-  function changeHandler(e) {
-    setState({ ...state, textBox: e.target.value });
+  changeHandler(e) {
+    this.setState({  textBox: e.target.value });
   }
-  let endTime=Date.now() + ShowToDur(state.textBox)
-  const audio = new Audio(sound)
-  
-  function ssHandler() {
-    endTime = Date.now() + ShowToDur(state.textBox) ;    
-    setState({...state,running:!state.running})    
+  ssHandler() {
+    this.endTime = Date.now() + this.ShowToDur(this.state.textBox);
+    this.setState({ running: !this.state.running });
   }
-
-  return (
-    <div class="timer_card">
-      <form onSubmit={e => e.preventDefault()}>
-        <input
-          pattern="^\d{1,2}[:.]\d{1,2}$"
-          onChange={e => changeHandler(e)}
-          value={state.textBox}
-        />
-      </form>
-      <div onClick={e => ssHandler()}>start</div>
-      <div>end</div>
-      <div>config</div>
-    </div>
-  );
+  resetHandler(){
+    this.setState({textBox:this.durToShow(this.state.duration)})
+    this.state.audio.pause()
+    
+  }
+  render() {
+    return (
+      <div class="timer_card">
+        <form onSubmit={e => e.preventDefault()}>
+          <input
+            pattern="^\d{1,2}[:.]\d{1,2}$"
+            onChange={e => this.changeHandler(e)}
+            value={this.state.textBox}
+          />
+        </form>
+        <div onClick={e => this.ssHandler(e)}>start</div>
+        <div onClick={e => this.resetHandler(e)}>reset</div>
+        <div>config</div>
+      </div>
+    );
+  }
 }
 export default Card;
