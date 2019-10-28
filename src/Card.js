a92e6ce458620ab859c7425cd382395ce8c80ead
roshad <roshad87a9@gmail.com>
2019-10-28 13:21:44 +0800

    为何不会停，反而闪？

--- src/Card.js
+++ src/Card.js
@@ -3,34 +3,25 @@ import "./Card.css";
 import sound from "./alarm.mp3";
 
 function Card() {
-  const [state, setState] = useState({
-    duration: 300000,
+  const [state, setState] = useState({    
     textBox: "5:0"
   });
   function durToShow(dur) {
     return Math.floor(dur / 60000) + ":" + Math.floor((dur % 60000) / 1000);
   }
+  function ShowToDur(text){
+      const matched = text.match(/^(\d{1,2}):(\d{1,2})$/)
+      return matched[1] * 60000 + matched[2] * 1000
+  }
   function changeHandler(e) {
     setState({ ...state, textBox: e.target.value });
-  }
-  function submitHandler(e) {
-    e.preventDefault();
-    const matched = e.target
-      .getElementsByTagName("input")[0]
-      .value.match(/^(\d{1,2})[:.](\d{1,2})$/);
-    let pri_dur = matched[1] * 60000 + matched[2] * 1000;
-    setState({
-      ...state,
-      duration: pri_dur,
-      textBox: durToShow(pri_dur)
-    });
-  }
-  function ssHandler(e) {
-    let endTime = Date.now() + state.duration,
+  }  
+  function ssHandler() {    
+    let endTime = Date.now() + ShowToDur(state.textBox),
       running = false;
     const audio = new Audio(sound),
       countDown = setInterval(() => {
-        if (state.running === true) {
+        if (running === true) {
           let remaining = endTime - Date.now();
           remaining = remaining < 0 ? 0 : remaining;
           setState({
@@ -43,25 +34,20 @@ function Card() {
           }
         }
       }, 100);
-    if (running === false) {
-        endTime = Date.now() + state.duration
-
-    } else {
-      duration-=endTime-Date.now()
-    }
-    running =!running
+    
+    running=!running
   }
 
   return (
     <div class="timer_card">
-      <form onSubmit={e => submitHandler(e)}>
+      <form onSubmit={e => e.preventDefault()}>
         <input
           pattern="^\d{1,2}[:.]\d{1,2}$"
           onChange={e => changeHandler(e)}
           value={state.textBox}
         />
       </form>
-      <div onClick={e => ssHandler(e)}>start</div>
+      <div onClick={e => ssHandler()}>start</div>
       <div>end</div>
       <div>config</div>
     </div>
